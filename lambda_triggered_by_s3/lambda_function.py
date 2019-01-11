febb6ef9cf25d6907b223ddb4fc06b56563d80f3
souadmabrouk1990 <46601271+souadmabrouk1990@users.noreply.github.com>
2019-01-11 15:30:58 +0100

    Create lambda_function.py

---
+++ lambda_triggered_by_s3/lambda_function.py
@@ -0,0 +1,32 @@
+import boto3
+import uuid
+
+# Créer les clients boto3 pour avoir une connexion vers les APIs AWS
+s3_client = boto3.client('s3')
+sns_client = boto3.client('sns')
+
+def lambda_handler(event, context):
+    print("Start function")
+    # pour chaque evenement (ici fichier uploadé sur S3)
+    for record in event['Records']:
+        # Récupérer le nom du bucket et le nom du fichier
+        bucket = record['s3']['bucket']['name']
+        key = record['s3']['object']['key'] 
+        print("Bucket name:" + bucket)
+        print("Key name:" + key)
+        # Construire le path pour le fichier telechargé
+        download_path = '/tmp/{}{}'.format(uuid.uuid4(), key)
+        # Télécharger le fichier
+        s3_client.download_file(bucket, key, download_path)
+        print("File is downloaded to the path" + download_path)
+        # Lire le fichier et recupérer le contenu
+        f= open(download_path,"r") 
+        content_file = f.read()
+        # Afficher le contenu du fichier
+        print(content_file)
+        # Si le mot "Booked" apparait dans le fichier, envoyer une notification
+        if ("booked") in content_file:
+            sns_client.publish(TopicArn='arn:aws:sns:eu-west-1:707529749811:topic-lambda-file-processing',
+                               Message='You successfuly booked your trip')
+            print("Notifications are successfuly sent to the sns topic subscriptions")
+    return True
